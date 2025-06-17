df4a08c5136f92a5ef47b0eae53cf9aba6543dca
«Alla» <alladavydova86@gmail.com>
2025-06-17 06:44:02 +0300

    lesson_3

--- 02_Lesson/lesson_2_task_3.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def square(side):
-    area = side ** 2
-    return math.ceil(area)
-
-print(square(5))
-print(square(4.2))
-print(square(3.1))
--- 03_Lesson/lesson_3_task_2.py
+++ 03_Lesson/lesson_3_task_2.py
@@ -1,8 +1,4 @@
-class Smartphone:
-    def __init__(self, brand, model, phone_number):
-        self.brand = brand
-        self.model = model
-        self.phone_number = phone_number
+from smartphone import Smartphone
 
 
 catalog = [
