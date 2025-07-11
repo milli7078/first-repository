92a832925030ecfbbb7b57d52a6c62a769ea4c9e
«Alla» <alladavydova86@gmail.com>
2025-07-11 11:35:59 +0300

    6 lesson

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,30 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_form_validation():
+    driver = webdriver.Chrome()
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+    driver.find_element(By.NAME, "first-name").send_keys("Иван")
+    driver.find_element(By.NAME, "last-name").send_keys("Петров")
+    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
+
+    wait = WebDriverWait(driver, 10)
+    email_input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='email']")))
+    email_input.send_keys("test@skypro.com")
+
+    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
+    driver.find_element(By.NAME, "city").send_keys("Москва")
+    driver.find_element(By.NAME, "country").send_keys("Россия")
+    driver.find_element(By.NAME, "job-position").send_keys("QA")
+    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+
+    driver.find_element(By.TAG_NAME, "form").submit()
+
+    time.sleep(5)
+    driver.quit()
