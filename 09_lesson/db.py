5bcfbdc9c06c07c6e1c6196cb5d8025c19845783
«Alla» <alladavydova86@gmail.com>
2025-08-03 07:28:01 +0300

    9

--- 09_lesson/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-# Подставь свои реальные данные ниже:
-DATABASE_URL = "postgresql://myuser:mypassword@localhost:5432/mydatabase"
-
-engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
